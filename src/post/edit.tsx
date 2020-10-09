f87909a168f7edfd733edf3e7599db7c7f74796e
Igor Sukharev <imixaly4@gmail.com>
2020-10-09 16:12:22 +0200

    add dialog with autocmplete input

--- src/post/edit.tsx
+++ src/post/edit.tsx
@@ -9,31 +9,55 @@ import {
   EditProps,
 } from 'react-admin';
 
-export const PostEdit: FC<EditProps> = props => (
-  <Edit {...props}>
-      <SimpleForm>
-          <AutocompleteInput
-            // label="url4"
-            source="url555"
-            choices={[
-                { id: 'programming', name: 'Programming' },
-                { id: 'lifestyle', name: 'Lifestyle' },
-                { id: 'photography', name: 'Photography' }
-            ]}
-            // translateChoice={false}
-            // options={{
-            //     suggestionsContainerProps: {
-            //         zIndex: 2000
-            //     }
-            // }}
-          />
+import {
+  Dialog,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-          <ReferenceInput source="userId" reference="users">
-            <SelectInput optionText="name" />
-          </ReferenceInput>
-          <TextInput source="id" />
-          <TextInput source="title" />
-          <TextInput multiline source="body" />
-      </SimpleForm>
-  </Edit>
-);
+const useStyles = makeStyles((theme) => ({
+  suggestionsContainerOpen: {
+    zIndex: 2000,
+  }
+}));
+
+export const PostEdit: FC<EditProps> = props => {
+  const classes = useStyles();
+
+  return (
+    <Edit {...props}>
+        <SimpleForm>
+            <AutocompleteInput
+              source="url555"
+              choices={[
+                  { id: 'programming', name: 'Programming' },
+                  { id: 'lifestyle', name: 'Lifestyle' },
+                  { id: 'photography', name: 'Photography' }
+              ]}
+              translateChoice={false}
+            />
+
+            <Dialog open>
+              <AutocompleteInput
+                source="url777"
+                choices={[
+                    { id: 'programming', name: 'Programming' },
+                    { id: 'lifestyle', name: 'Lifestyle' },
+                    { id: 'photography', name: 'Photography' }
+                ]}
+                classes={{
+                  suggestionsContainer: classes.suggestionsContainerOpen,
+                  suggestionsContainerOpen: classes.suggestionsContainerOpen,
+                }}
+              />
+            </Dialog>
+
+            <ReferenceInput source="userId" reference="users">
+              <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="id" />
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+  );
+};
